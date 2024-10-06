faf9b181fc771d4960182efd90b922bfb60fcce1
Igor <dumanschi.igor@gmail.com>
2024-10-06 17:31:30 +0300

    JS_3_HW

---
+++ main.js
@@ -0,0 +1,22 @@
+const countBE = 21900;
+const hour = 60;
+const day = 1440;
+const week = 10080;
+
+if (countBE / week >= 1) {
+  const ramainingWeeks = countBE / week;
+  const ramainngDaysInMinutes = countBE % week;
+  console.log(
+    `${Math.floor(ramainingWeeks)}w ${Math.floor(ramainngDaysInMinutes / day)}d`
+  );
+} else if (countBE / day >=1) {
+	const remainingDays = countBE / day
+	const remainingHoursTransform = countBE % day
+	console.log(`${Math.floor(remainingDays)}d ${Math.floor(remainingHoursTransform / hour)}h `)
+} else if (countBE / hour >= 1) {
+	const remainigHours = countBE / hour
+	const remainingMinutesTransform = countBE % hour
+	console.log(`${Math.floor(remainigHours)}h ${remainingMinutesTransform}m`)
+} else if (countBE <= 60) {
+	console.log(`less than an hour`)
+}
